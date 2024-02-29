d34f1a460d26699feccffb4750fbca3791d8b2fb
persival001 <persival@free.fr>
2024-02-29 17:39:30 +0100

    Ignore gradle.properties and remove it from index

--- app/src/main/java/com/persival/k_lory/ui/main/ApiUIState.kt
+++
@@ -1,9 +0,0 @@
-package com.persival.k_lory.ui.main
-
-import com.persival.k_lory.domain.food_facts.model.FoodPropertiesEntity
-
-sealed class ApiUIState {
-    object Loading : ApiUIState()
-    data class Success(val products: List<FoodPropertiesEntity>) : ApiUIState()
-    data class Error(val message: String) : ApiUIState()
-}
