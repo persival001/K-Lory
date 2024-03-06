22e3407ba31e8677d788b476c5a0ae5ab6932614
persival001 <persival@free.fr>
2024-03-06 08:40:45 +0100

    Version 1.0 some improvements

--- app/src/main/java/com/persival/k_lory/ui/composables/ProductCard.kt
+++ app/src/main/java/com/persival/k_lory/ui/composables/ProductCard.kt
@@ -7,7 +7,9 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.persival.k_lory.R
 import com.persival.k_lory.domain.food_facts.model.FoodPropertiesEntity
 
 @Composable
@@ -22,15 +24,15 @@ fun ProductCard(product: FoodPropertiesEntity) {
         ) {
             Text(text = product.description ?: "")
             if (!product.ingredients.isNullOrBlank()) {
-                Text(text = "Ingredients: ${product.ingredients}")
+                Text(text = stringResource(id = R.string.ingredients, product.ingredients))
             }
             if (!product.servingSizeUnit.isNullOrBlank() && product.servingSize != null) {
-                Text(text = "Serving size: ${product.servingSize} ${product.servingSizeUnit}")
+                Text(text = stringResource(id = R.string.serving_size, product.servingSize, product.servingSizeUnit))
             }
-            Text(text = "Energy: ${product.energy}")
-            Text(text = "Protein: ${product.protein}")
-            Text(text = "Carbohydrate: ${product.carbohydrate}")
-            Text(text = "Fat: ${product.fat}")
+            Text(text = stringResource(id = R.string.energy, product.energy?.toString() ?: "N/A"))
+            Text(text = stringResource(id = R.string.protein, product.protein?.toString() ?: "N/A"))
+            Text(text = stringResource(id = R.string.carbohydrate, product.carbohydrate?.toString() ?: "N/A"))
+            Text(text = stringResource(id = R.string.fat, product.fat?.toString() ?: "N/A"))
         }
     }
 }
